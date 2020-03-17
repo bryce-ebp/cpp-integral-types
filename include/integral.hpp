89c0b0de7f0310ac875261dbd32b12f04235d14f
Bryce <bbroom@hpsstudents.net>
2020-03-16 22:50:09 -0400

    Initial commit

---
+++ include/integral.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <numeric>
+
+/* https://doc.rust-lang.org/1.30.0/std/primitive.i8.html
+ * https://doc.rust-lang.org/1.30.0/std/primitive.u8.html
+ *
+ * */
+
+template<typename T>
+class integral {
+public:
+    constexpr integral() = default;
+    constexpr integral(const T t) noexcept
+        : value_(t) { }
+
+    static constexpr T get_max() noexcept { return std::numeric_limits<T>().max(); }
+    static constexpr T get_min() noexcept { return std::numeric_limits<T>().min(); }
+
+    constexpr operator T() const noexcept { return value_; }
+
+private:
+    T value_{ };
+};
+
+using i8 = integral<char>;
+using u8 = integral<unsigned char>;
+
+using i16 = integral<short>;
+using u16 = integral<unsigned short>;
+
+using i32 = integral<int>;
+using u32 = integral<unsigned int>;
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "integral.hpp"
+
+int main() {
+    i32 var { 100 };
+
+    std::cout << var;
+
+    return 0;
+}
