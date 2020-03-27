e43c8f48432369ee5428847be4c83a310bc5aa15
Bryce <bbroom@hpsstudents.net>
2020-03-27 10:03:20 -0400

    Added more test cases, and more functions.
    Functions added from the rust documentation.
    
    TODO: decrease size of the integers.

--- include/integral.hpp
+++ include/integral.hpp
@@ -2,6 +2,9 @@
 
 #include <numeric>
 #include <type_traits>
+#include <bitset>
+#include <cctype>
+#include <algorithm>
 
 /* https://doc.rust-lang.org/1.30.0/std/primitive.i8.html
  * https://doc.rust-lang.org/1.30.0/std/primitive.u8.html
@@ -14,17 +17,103 @@ concept integral_type = std::is_integral_v<T>;
 template<integral_type T>
 class integral {
 public:
-    constexpr integral() = default;
+    constexpr integral() noexcept = default;
     constexpr integral(const T t) noexcept
         : value_(t) { }
 
-    static constexpr T get_max() noexcept { return std::numeric_limits<T>().max(); }
-    static constexpr T get_min() noexcept { return std::numeric_limits<T>().min(); }
-
     constexpr operator T() const noexcept { return value_; }
+
+    static constexpr T get_max() noexcept { return limit_.max(); }
+    static constexpr T get_min() noexcept { return limit_.min(); }
+
+    constexpr std::bitset<sizeof(T) * 8> &get_bitset() const noexcept { return bitset_; }
+
+    [[nodiscard]] std::string bitset_str() const noexcept {
+        return bitset_.to_string();
+    }
+
+    [[nodiscard]] constexpr unsigned short count_ones() const noexcept {
+        return get_bitset().count();
+    }
+
+    [[nodiscard]] constexpr unsigned short count_zeros() const noexcept {
+        return bitset_size() - count_ones();
+    }
+
+    [[nodiscard]] constexpr unsigned short bitset_size() const noexcept {
+        return get_bitset().size();
+    }
+
+    constexpr T leading_zeros() const noexcept {
+        const auto str{ bitset_str() };
+
+        return std::distance(str.begin(), std::adjacent_find(str.begin(), str.end(), [] (const auto &lhs, const auto &rhs) {
+            return lhs != rhs;
+        })) + 1;
+    }
+
+    constexpr T trailing_zeros() const noexcept {
+        const auto str{ bitset_str() };
+
+        return std::distance(str.rbegin(), std::adjacent_find(str.rbegin(), str.rend(), [] (const auto &lhs, const auto &rhs) {
+            return lhs != rhs;
+        })) + 1;
+    }
+
+    [[nodiscard]] constexpr bool is_pow_2() const noexcept {
+        return value_ && !(value_ & (value_ - 1));
+    }
+
+    [[nodiscard]] constexpr bool is_ascii() const noexcept {
+        return value_ < 256;
+    }
+
+    [[nodiscard]] constexpr bool is_ascii_type() const noexcept {
+        return get_max() < 256;
+    }
+
+    [[nodiscard]] constexpr bool is_ascii_lowercase() const noexcept {
+        return std::islower(value_);
+    }
+
+    [[nodiscard]] constexpr bool is_ascii_uppercase() const noexcept {
+        return std::isupper(value_);
+    }
+
+    // Good post related to pow: https://www.rookieslab.com/posts/fast-power-algorithm-exponentiation-by-squaring-cpp-python-implementation
+    [[nodiscard]] constexpr long long pow(long long exp) const noexcept {
+        auto ret{ 1 };
+        auto base{ value_ };
+
+        while (exp > 0) {
+            if (exp & 1) {
+                // Magic number explanation: https://codeaccepted.wordpress.com/2014/02/15/output-the-answer-modulo-109-7/
+                ret = (ret * base) % 1000000007;
+            }
+
+            base = (base * base) % 1000000007;
+            exp >>= 1;
+        }
+
+        return ret;
+    }
+
+    [[nodiscard]] constexpr T next_pow_2() const noexcept {
+        auto ret{ value_ };
+        --ret;
+        ret |= ret >> 1;
+        ret |= ret >> 2;
+        ret |= ret >> 4;
+        ret |= ret >> 8;
+        ret |= ret >> 16;
+        return ++ret;
+    }
 
 private:
     T value_{ };
+    static constexpr auto limit_{ std::numeric_limits<T>() };
+    std::bitset<sizeof(T) * 8> bitset_{ value_ };
+
 };
 
 using i8 = integral<char>;
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,43 @@
 #include <iostream>
+#include <chrono>
 
 #include "integral.hpp"
 
-int main() {
-    i32 var { 100 };
 
-    std::cout << var;
+int main() {
+    constexpr u16 test { 60 };
+
+    constexpr i32 var { 8 };
+    u16 var2 {240 };
+    i32 var3{ var + var2 };
+
+    const auto prev_time = std::chrono::steady_clock::now();
+
+    std::cout << "var: " << var << '\n'
+            << "var get_max: " << var.get_max() << '\n'
+            << "var get_min: " << var.get_min() << '\n'
+            << "var is_pow_2: " << var.is_pow_2() << "\n\n\n"
+
+
+            << "test: " << test << '\n'
+            << "test pow: " << test.pow(3) << '\n'
+            << "test leading_zeros: " << test.leading_zeros() << '\n'
+            << "test trailing_zeros: " << test.trailing_zeros() << '\n'
+            << "test sizeof(bitset): " << sizeof(u32) << '\n'
+            << "test bitset_to_string: " << test.bitset_str() << "\n\n\n"
+
+
+            << "var2: " << var2 << '\n'
+            << "var2: get_max: " << var2.get_max() << '\n'
+            << "var2: get_min: " << var2.get_min() << '\n'
+            << "var2: next_pow_2: " << var2.next_pow_2() << '\n'
+            << "var2 is_pow2: " << var2.is_pow_2() << "\n\n\n"
+
+
+            << "var3: " << var3 << '\n'
+            << "var3 get_max: " << var3.get_max() << '\n'
+            << "var3 get_min: " << var3.get_min() << '\n'
+            << "execution: " << std::chrono::duration_cast<std::chrono::duration<float>>(std::chrono::steady_clock::now() - prev_time).count();
 
     return 0;
 }
